4627aecc8a11c37fc3719f1952d4764a51f78442
Lennard <lennard.johannes.hurst@hs-furtwangen.de>
2020-05-27 14:35:08 +0200

    Aufgabe 05
    Startschuss

--- Steckbrief/Aufgaben/Aufgabe05/script.js
+++ Steckbrief/Aufgaben/Aufgabe05/script.js
@@ -1,3 +1,9 @@
 "use strict";
-console.log("Hallo Wel hupsi");
+console.log("Hallo Welt");
+let kartoffel = {
+    Name: "Linda",
+    Description: "1 Kartoffel",
+    price: 3
+};
+Array;
 //# sourceMappingURL=script.js.map
--- Steckbrief/Aufgaben/Aufgabe05/script.ts
+++ Steckbrief/Aufgaben/Aufgabe05/script.ts
@@ -1,1 +1,14 @@
-console.log("Hallo Wel hupsi");
+console.log("Hallo Welt");
+interface Product {
+    Name: String;
+    Description: String;
+    price: Number;
+}
+
+let kartoffel: Product = {
+    Name: "Linda",
+    Description: "1 Kartoffel",
+    price: 3
+}
+
+Array
