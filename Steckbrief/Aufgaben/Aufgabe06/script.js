0a1eb9affb5a751e9ae5eeec06e9169597e9cb36
Lennard <lennard.johannes.hurst@hs-furtwangen.de>
2020-06-08 21:19:58 +0200

    Aufgabe 06 überarbeitung

--- Steckbrief/Aufgaben/Aufgabe06/script.js
+++ Steckbrief/Aufgaben/Aufgabe06/script.js
@@ -1,16 +1,12 @@
 "use strict";
 var Aufgabe06;
 (function (Aufgabe06) {
-   
     //#region Einfügen in Die Kategorien
-   
     // Tut mir leid wer sich das hier anschauen muss
-   
     //For-schleife für die Einsortierung in die Kategorie
     let _kategorie = "festkochend";
     let productCounter = document.createElement("div");
     productCounter.setAttribute("id", "productCounter");
-   
     //Eventlistener zu den einzelnen Elementen hinzufügen
     document.getElementById("konto")?.appendChild(productCounter);
     document.getElementById("zuKartoffel1")?.addEventListener("click", auswahlEinschreanken);
@@ -25,15 +21,12 @@ var Aufgabe06;
     document.getElementById("showAll")?.addEventListener("click", auswahlEinschreanken);
     document.getElementById("zumWarenkorb")?.addEventListener("click", warenkorbAufbauen);
     document.getElementById("warenkorbInhalt")?.setAttribute("style", "display : none");
-  
     //Variablen deklarieren und initialisieren
     let gesamtPreis = 0;
     let counter = 0;
-   
     //Produkte, zum Verkauf stehen erstellen
     for (let i = 0; i < Aufgabe06.imVerkauf.length; i++) {
         productCounter.style.display = "none";
-    
         //Einsortierung in die passende Kategorie
         if (Aufgabe06.imVerkauf[i].Kategorie == "festkochend") {
             _kategorie = "festkochend";
@@ -44,59 +37,49 @@ var Aufgabe06;
         else {
             _kategorie = "exoten";
         }
-    
         //Estellen von Div Elementen 
         let newDiv = document.createElement("div");
-   
         //Div id zuweisen
-        newDiv.id = "artikel" + _kategorie + i;
-     
+        newDiv.id = "artikel" + i;
+        newDiv.setAttribute("kategorie", _kategorie.toString());
         //Element hinzufügen
         document.getElementById(_kategorie)?.appendChild(newDiv);
-      
         //Bild hinzufügen
         let newImg = document.createElement("img");
         newImg.src = Aufgabe06.imVerkauf[i].imgSrc;
         newImg.setAttribute("alt", Aufgabe06.imVerkauf[i].Art);
         newDiv.appendChild(newImg);
-      
         //Label hinzufügen
         let newL = document.createElement("label");
         newL.setAttribute("for", Aufgabe06.imVerkauf[i].Name);
         newL.innerHTML = "Kilogramm: ";
         newDiv.appendChild(newL);
-      
         //Dropdownmenu hinzufügen
         let newSelect = document.createElement("select");
         newSelect.setAttribute("class", "option");
         newSelect.name = Aufgabe06.imVerkauf[i].Name;
         newSelect.id = Aufgabe06.imVerkauf[i].Name + "select";
         newDiv.appendChild(newSelect);
-     
         //Option vom Dropdownmenu
         let newOp1 = document.createElement("option");
         newOp1.value = "2.5";
         newOp1.innerHTML = "5 kg | " + Aufgabe06.imVerkauf[i].price1 + "€";
         let newOp2 = document.createElement("option");
-      
         // newOp2.setAttribute("class", "option");
         newOp2.value = "5";
         newOp2.innerHTML = "10 kg | " + Aufgabe06.imVerkauf[i].price2 + "€";
         newSelect.appendChild(newOp1);
         newSelect.appendChild(newOp2);
-      
         //Name hinzugefügt 
         let newName = document.createElement("p");
         newName.setAttribute("class", "Name");
         newName.innerText = Aufgabe06.imVerkauf[i].Name;
         newDiv.appendChild(newName);
-       
         //Beschreibung hinzugefügt 
         let newP = document.createElement("p");
         newP.setAttribute("class", "beschreibung");
         newP.innerHTML = Aufgabe06.imVerkauf[i].Description;
         newDiv.appendChild(newP);
-      
         //Button hinzugefügt 
         let newB = document.createElement("input");
         newB.addEventListener("click", handlerWarenkorb);
@@ -106,9 +89,8 @@ var Aufgabe06;
         newB.setAttribute("articleIndex", i.toString());
         newDiv.appendChild(newB);
     }
-  
-  
-    //Funktion um Preis des Warenkorbs zu berechnen, klappt aber nicht, da Elemente nicht aus dem Array gelöscht werden können
+    //Funktion um Preis des Warenkorbs zu berechnen,
+    // klappt aber nur Teilweise, da sie manchmal auf einen Index im Array zugreifen will der nicht mehr da ist
     function preisBerechnung() {
         let preis = 0;
         for (let i = 0; i < Aufgabe06.warenkorb.length; i++) {
@@ -116,8 +98,6 @@ var Aufgabe06;
         }
         return preis;
     }
-   
-   
     //Funktion um den Elemente dem Warenkorb hinzu zu fügen
     function handlerWarenkorb(_kaufen) {
         counter += 1;
@@ -138,8 +118,6 @@ var Aufgabe06;
         productCounter.style.display = "block";
         productCounter.innerHTML = "" + counter;
     }
-   
-   
     //Filtert die anderen Kategorien aus bzw. lässt Kategorien aus- und einblenden
     function auswahlEinschreanken(_event) {
         if (counter == 0) {
@@ -147,43 +125,64 @@ var Aufgabe06;
         }
         let target = _event.target;
         let kategorie = target.getAttribute("href");
-        switch (kategorie) {
-            case "#1kartoffel": {
-                document.getElementById("kartoffel1")?.setAttribute("style", "display : block");
-                document.getElementById("kartoffel2")?.setAttribute("style", "display : none");
-                document.getElementById("kartoffel3")?.setAttribute("style", "display : none");
-                document.getElementById("showAll")?.setAttribute("style", "display : block");
-                document.getElementById("warenkorbInhalt")?.setAttribute("style", "display : none");
-                break;
+        for (let i = 0; i < Aufgabe06.imVerkauf.length; i++) {
+            let artikelKategorie = document.getElementById("artikel" + i)?.getAttribute("kategorie");
+            if (kategorie == "#" + artikelKategorie) {
+                document.getElementById("artikel" + i)?.setAttribute("style", "display : block");
+                console.log("Erster fall");
             }
-            case "#2kartoffel": {
-                document.getElementById("kartoffel1")?.setAttribute("style", "display : none");
-                document.getElementById("kartoffel2")?.setAttribute("style", "display : block");
-                document.getElementById("kartoffel3")?.setAttribute("style", "display : none");
-                document.getElementById("showAll")?.setAttribute("style", "display : block");
-                document.getElementById("warenkorbInhalt")?.setAttribute("style", "display : none");
-                break;
+            else {
+                document.getElementById("artikel" + i)?.setAttribute("style", "display : none");
+                console.log("Zweiter fall");
             }
-            case "#3kartoffel": {
-                document.getElementById("kartoffel1")?.setAttribute("style", "display : none");
-                document.getElementById("kartoffel2")?.setAttribute("style", "display : none");
-                document.getElementById("kartoffel3")?.setAttribute("style", "display : block");
-                document.getElementById("showAll")?.setAttribute("style", "display : block");
-                document.getElementById("warenkorbInhalt")?.setAttribute("style", "display : none");
-                break;
-            }
-            default: {
-                document.getElementById("kartoffel1")?.setAttribute("style", "display : block");
-                document.getElementById("kartoffel2")?.setAttribute("style", "display : block");
-                document.getElementById("kartoffel3")?.setAttribute("style", "display : block");
-                document.getElementById("showAll")?.setAttribute("style", "display : none");
-                document.getElementById("warenkorbInhalt")?.setAttribute("style", "display : none");
-                break;
+            if (kategorie == null) {
+                document.getElementById("artikel" + i)?.setAttribute("style", "display : block");
+                console.log("Driiter fall");
             }
         }
+        switch (kategorie) {
+            case "#festkochend":
+                document.getElementById("1kartoffel")?.setAttribute("style", "display : block");
+                document.getElementById("2kartoffel")?.setAttribute("style", "display : none");
+                document.getElementById("3kartoffel")?.setAttribute("style", "display : none");
+                document.getElementById("showAll")?.setAttribute("style", "display : block");
+                document.getElementById("kartoffel1")?.setAttribute("style", "display : block");
+                document.getElementById("kartoffel2")?.setAttribute("style", "display : block");
+                document.getElementById("kartoffel3")?.setAttribute("style", "display : block");
+                document.getElementById("warenkorbInhalt")?.setAttribute("style", "display : none");
+                break;
+            case "#mehligkochend":
+                document.getElementById("1kartoffel")?.setAttribute("style", "display : none");
+                document.getElementById("2kartoffel")?.setAttribute("style", "display : block");
+                document.getElementById("3kartoffel")?.setAttribute("style", "display : none");
+                document.getElementById("showAll")?.setAttribute("style", "display : block");
+                document.getElementById("kartoffel1")?.setAttribute("style", "display : block");
+                document.getElementById("kartoffel2")?.setAttribute("style", "display : block");
+                document.getElementById("kartoffel3")?.setAttribute("style", "display : block");
+                document.getElementById("warenkorbInhalt")?.setAttribute("style", "display : none");
+                break;
+            case "#exoten":
+                document.getElementById("1kartoffel")?.setAttribute("style", "display : none");
+                document.getElementById("2kartoffel")?.setAttribute("style", "display : none");
+                document.getElementById("3kartoffel")?.setAttribute("style", "display : block");
+                document.getElementById("showAll")?.setAttribute("style", "display : block");
+                document.getElementById("kartoffel1")?.setAttribute("style", "display : block");
+                document.getElementById("kartoffel2")?.setAttribute("style", "display : block");
+                document.getElementById("kartoffel3")?.setAttribute("style", "display : block");
+                document.getElementById("warenkorbInhalt")?.setAttribute("style", "display : none");
+                break;
+            default:
+                document.getElementById("1kartoffel")?.setAttribute("style", "display : block");
+                document.getElementById("2kartoffel")?.setAttribute("style", "display : block");
+                document.getElementById("3kartoffel")?.setAttribute("style", "display : block");
+                document.getElementById("showAll")?.setAttribute("style", "display : none");
+                document.getElementById("kartoffel1")?.setAttribute("style", "display : block");
+                document.getElementById("kartoffel2")?.setAttribute("style", "display : block");
+                document.getElementById("kartoffel3")?.setAttribute("style", "display : block");
+                document.getElementById("warenkorbInhalt")?.setAttribute("style", "display : none");
+                break;
+        }
     }
-   
-   
     //Warenkorb aufbauen und updaten 
     function warenkorbAufbauen() {
         document.getElementById("kartoffel1")?.setAttribute("style", "display : none");
@@ -193,36 +192,29 @@ var Aufgabe06;
         for (let i = 0; i < Aufgabe06.warenkorb.length; i++) {
             if (!document.getElementById("WarenkorbItem" + i.toString())) {
                 if (Aufgabe06.warenkorb[i] != null) {
-                   
                     //Estellen von Div Elementen 
                     let newDiv = document.createElement("div");
-                   
                     //Div id zuweisen
                     newDiv.setAttribute("articleIndex", i.toString());
                     newDiv.setAttribute("id", "WarenkorbItem" + i.toString());
-                   
                     //Element hinzufügen
                     document.getElementById("Angebot")?.appendChild(newDiv);
-                   
                     //Bild hinzufügen
                     let newImg = document.createElement("img");
                     newImg.src = Aufgabe06.warenkorb[i].imgSrc;
                     newImg.setAttribute("alt", Aufgabe06.warenkorb[i].Art);
                     newDiv.appendChild(newImg);
-                  
                     //Label hinzufügen
                     let newL = document.createElement("label");
                     newL.setAttribute("for", Aufgabe06.warenkorb[i].Name);
                     newL.innerHTML = "Kilogramm:";
                     newDiv.appendChild(newL);
-                  
                     //Dropdownmenu hinzufügen
                     let newSelect = document.createElement("select");
                     newSelect.setAttribute("class", "option");
                     newSelect.name = Aufgabe06.warenkorb[i].Name;
                     newSelect.id = Aufgabe06.warenkorb[i].Name + "select";
                     newDiv.appendChild(newSelect);
-                  
                     //Option vom Dropdownmenu
                     let newOp1 = document.createElement("option");
                     newOp1.value = "2.5";
@@ -232,19 +224,16 @@ var Aufgabe06;
                     newOp2.innerHTML = "5 kg | " + Aufgabe06.warenkorb[i].price2 + "€";
                     newSelect.appendChild(newOp1);
                     newSelect.appendChild(newOp2);
-                 
                     //Name hinzugefügt 
                     let newName = document.createElement("p");
                     newName.setAttribute("class", "Name");
                     newName.innerText = Aufgabe06.warenkorb[i].Name;
                     newDiv.appendChild(newName);
-                   
                     //Beschreibung hinzugefügt 
                     let newP = document.createElement("p");
                     newP.setAttribute("class", "beschreibung");
                     newP.innerHTML = Aufgabe06.warenkorb[i].Description;
                     newDiv.appendChild(newP);
-                  
                     //Button hinzugefügt 
                     let newB = document.createElement("input");
                     newB.addEventListener("click", handlerWarenkorbEntfernen);
@@ -263,9 +252,7 @@ var Aufgabe06;
                 document.getElementById("4kartoffel").innerHTML = "Aktueller Betrag: " + gesamtPreis.toFixed(2).toString() + "€" + "<br> <input type='button' id='Bestellen' value='Bestellen'>";
             }
         }
-        
-        
-    // Produkte aus dem Warenkorb nehmen und aktuellen Preis berechnen
+        // Produkte aus dem Warenkorb nehmen und aktuellen Preis berechnen
     }
     function handlerWarenkorbEntfernen(_kaufen) {
         if (counter > 0)
@@ -300,8 +287,6 @@ var Aufgabe06;
         }
         warenkorbAufbauen();
     }
-    
     console.log("Fertig geladen");
-
 })(Aufgabe06 || (Aufgabe06 = {}));
 //# sourceMappingURL=script.js.map
