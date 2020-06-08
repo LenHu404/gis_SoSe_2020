804a348900af332d61047f3df1c1e7c091e5014b
Lennard <lennard.johannes.hurst@hs-furtwangen.de>
2020-06-08 22:39:44 +0200

    Aufgabe 06
    Sofie ist aufmerksam

--- Steckbrief/Aufgaben/Aufgabe06/script.ts
+++ Steckbrief/Aufgaben/Aufgabe06/script.ts
@@ -259,7 +259,7 @@ namespace Aufgabe06 {
                     newOp1.innerHTML = "5 kg | " + warenkorb[i].price1 + "€";
                     let newOp2: HTMLOptionElement = document.createElement("option");
                     newOp2.value = "5";
-                    newOp2.innerHTML = "5 kg | " + warenkorb[i].price2 + "€";
+                    newOp2.innerHTML = "10 kg | " + warenkorb[i].price2 + "€";
                     newSelect.appendChild(newOp1);
                     newSelect.appendChild(newOp2);
 
