a2ea21e12c20f1e76478fc4559ca303c2adc9d75
Lennard <lennard.johannes.hurst@hs-furtwangen.de>
2020-06-18 17:20:54 +0200

    Aufgabe 08
    Aufsetzten

---
+++ Steckbrief/Aufgaben/Aufgabe08/server.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Aufgabe08 = void 0;
+const Http = require("http");
+var Aufgabe08;
+(function (Aufgabe08) {
+    Http.createServer();
+})(Aufgabe08 = exports.Aufgabe08 || (exports.Aufgabe08 = {}));
+//# sourceMappingURL=server.js.map
---
+++ Steckbrief/Aufgaben/Aufgabe08/server.ts
@@ -0,0 +1,6 @@
+import * as Http from "http";
+import * as Url from "url";
+
+export namespace Aufgabe08 {
+    Http.createServer();
+}
