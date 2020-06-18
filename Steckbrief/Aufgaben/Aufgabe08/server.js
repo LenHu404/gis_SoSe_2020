2fd8aec7be5c39462268801e3ddfc94eee75f090
Lennard <lennard.johannes.hurst@hs-furtwangen.de>
2020-06-19 00:18:16 +0200

    Aufgabe 08
    Start des Desasters

--- Steckbrief/Aufgaben/Aufgabe08/server.js
+++ Steckbrief/Aufgaben/Aufgabe08/server.js
@@ -17,9 +17,11 @@ var A08Server;
     }
     function handleRequest(_request, _response) {
         console.log("I hear voices!");
+        console.log("helo");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
+        _response.write("Es funktioniert!");
         _response.end();
     }
 })(A08Server = exports.A08Server || (exports.A08Server = {}));
