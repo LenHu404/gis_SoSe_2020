97c36deacb5cd42d30b0560e63bd31e151eab3f2
Lennard <lennard.johannes.hurst@hs-furtwangen.de>
2020-06-18 18:29:34 +0200

    Aufgabe 08
    huiuiui

--- Steckbrief/Aufgaben/Aufgabe08/server.js
+++ Steckbrief/Aufgaben/Aufgabe08/server.js
@@ -1,9 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Aufgabe08 = void 0;
+exports.A08Server = void 0;
 const Http = require("http");
-var Aufgabe08;
-(function (Aufgabe08) {
-    Http.createServer();
-})(Aufgabe08 = exports.Aufgabe08 || (exports.Aufgabe08 = {}));
+var A08Server;
+(function (A08Server) {
+    console.log("Starting server");
+    let port = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+    function handleListen() {
+        console.log("Listening");
+    }
+    function handleRequest(_request, _response) {
+        console.log("I hear voices!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.write(_request.url);
+        _response.end();
+    }
+})(A08Server = exports.A08Server || (exports.A08Server = {}));
 //# sourceMappingURL=server.js.map
